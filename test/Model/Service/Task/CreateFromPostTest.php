6fd2ec61898c8f599d82be5761c0554b78dd8b77
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-11-08 00:10:30 +0000

    Write preliminary create from post unit test

---
+++ test/Model/Service/Task/CreateFromPostTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace LeoGalleguillos\ProjectManagementTest\Model\Service;
+
+use LeoGalleguillos\Flash\Model\Service as FlashService;
+use LeoGalleguillos\ProjectManagement\Model\Service as ProjectManagementService;
+use LeoGalleguillos\ProjectManagement\Model\Table as ProjectManagementTable;
+use LeoGalleguillos\User\Model\Service as UserService;
+use PHPUnit\Framework\TestCase;
+
+class CreateFromPostTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->flashService = new FlashService\Flash();
+        $this->taskTableMock = $this->createMock(
+            ProjectManagementTable\Task::class
+        );
+        $this->loggedInUserServiceMock = $this->createMock(
+            UserService\LoggedInUser::class
+        );
+        $this->createFromPostService = new ProjectManagementService\Task\CreateFromPost(
+            $this->flashService,
+            $this->taskTableMock,
+            $this->loggedInUserServiceMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            ProjectManagementService\Task\CreateFromPost::class,
+            $this->createFromPostService
+        );
+    }
+}
