1c5b59b34374bf23df59cc250cbab4780aeb9885
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-11-14 19:23:21 +0000

    Write test for root relative url service

--- test/Model/Service/Task/RootRelativeUrlTest.php
+++ test/Model/Service/Task/RootRelativeUrlTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\ProjectManagementTest\Service\Task;
 
+use LeoGalleguillos\ProjectManagement\Model\Entity as ProjectManagementEntity;
 use LeoGalleguillos\ProjectManagement\Model\Service as ProjectManagementService;
 use LeoGalleguillos\String\Model\Service as StringService;
 use PHPUnit\Framework\TestCase;
@@ -20,6 +21,20 @@ class RootRelativeUrlTest extends TestCase
         $this->assertInstanceOf(
             ProjectManagementService\Task\RootRelativeUrl::class,
             $this->rootRelativeUrlService
+        );
+    }
+
+    public function testGetRootRelativeUrl()
+    {
+        $taskEntity = new ProjectManagementEntity\Task();
+        $taskEntity->setTaskId(54321)
+                   ->setName('This is the name!');
+
+        $rru = $this->rootRelativeUrlService->getRootRelativeUrl($taskEntity);
+
+        $this->assertSame(
+            '/project-management/tasks/54321/This-is-the-name',
+            $rru
         );
     }
 }
